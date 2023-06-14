977a51c3043f50abefaf05de5d281f460a66023d
Zaz Brown <zazbrown@zazbrown.com>
2023-06-14 07:35:47 +0000

    + lab1: diff: begin writing
    Currently just throws an exception unless 2 command-line arguments are recieved.

---
+++ 1/diff.cpp
@@ -0,0 +1,11 @@
+// finds the location of the first difference between two text files
+// Zaz Brown
+
+#include <iostream>
+
+int main(int argc, char* argv[]){
+  // throw an error if we are not given two command line arguments
+  if (argc != 3) {
+    throw std::invalid_argument("Invalid number of arguments");
+  }
+}
