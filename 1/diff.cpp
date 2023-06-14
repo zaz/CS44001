182aaa6fa49bdf708ecb28491dd676256da86996
Zaz Brown <zazbrown@zazbrown.com>
2023-06-14 11:01:20 +0000

    print the file name instead of file1, file2

--- 1/diff.cpp
+++ 1/diff.cpp
@@ -22,6 +22,20 @@ int main(int argc, char* argv[]){
   std::string file1 = argv[1];
   std::string file2 = argv[2];
 
+  // calculate the amount of left padding needed for printing the filenames
+  int filenameLengthDifference = file2.length() - file1.length();
+  auto file1Padding = std::string(std::max(0,  filenameLengthDifference), ' ');
+  auto file2Padding = std::string(std::max(0, -filenameLengthDifference), ' ');
+
+  // filename prefixes for later printing in a pretty way. e.g.:
+  //          file.txt:
+  // modified_file.txt:
+  auto file1Prefix = file1Padding + file1 + ": ";
+  auto file2Prefix = file2Padding + file2 + ": ";
+  //assert(file1Prefix.length() == file2Prefix.length());
+
+  int prefixLength = file1Prefix.length();
+
   // Open the files
   // XXX: Is there a better way than .fail() to check if the file is readable?
   std::ifstream stream1(file1);
@@ -35,8 +49,9 @@ int main(int argc, char* argv[]){
   // until we reach the end of one of the files
   while (getline(stream1, lineFrom1) && getline(stream2, lineFrom2)) {
     if (lineFrom1 != lineFrom2) {
-      std::cout << "file1: " << line_number << ": " << lineFrom1 << std::endl;
-      std::cout << "file2: " << line_number << ": " << lineFrom2 << std::endl;
+      auto prefix1 = file1Prefix + std::to_string(line_number) + ": ";
+      std::cout << prefix1 << lineFrom1 << std::endl;
+      std::cout << file2Prefix << line_number << ": " << lineFrom2 << std::endl;
 
       int minLineLength = std::min(lineFrom1.length(), lineFrom2.length());
       int i = 0;
@@ -45,7 +60,7 @@ int main(int argc, char* argv[]){
         ++i;
       }
       // print a ^ under the first character that differs
-      std::cout << std::string(10+i, ' ') << '^' << std::endl;
+      std::cout << std::string(prefix1.length()+i, ' ') << '^' << std::endl;
       // exit with return code 1 to indicate the files differ
       return 1;
     }
