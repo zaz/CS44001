52ba3368e5fe257ad319ce8de260db236d48db53
Zaz Brown <zazbrown@zazbrown.com>
2023-06-14 07:44:00 +0000

    + question: argc == argv.size()

--- 1/diff.cpp
+++ 1/diff.cpp
@@ -5,6 +5,7 @@
 
 int main(int argc, char* argv[]){
   // throw an error if we are not given two command line arguments
+  // XXX: Is it safe to assume argc == argv.size()?
   if (argc != 3) {
     std::cout << "ERROR: You must specify 2 files to diff" << std::endl;
     return 1;
