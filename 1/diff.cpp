236f64d69c62b5eb8445101a89bfc3811c7f9491
Zaz Brown <zazbrown@zazbrown.com>
2023-06-14 09:15:00 +0000

    find differing lines

--- 1/diff.cpp
+++ 1/diff.cpp
@@ -23,4 +23,16 @@ int main(int argc, char* argv[]){
   if (stream1.fail()) error("Could not open file " + file1);
   std::ifstream stream2(file2);
   if (stream2.fail()) error("Could not open file " + file2);
+
+  std::string lineFrom1;
+  std::string lineFrom2;
+  int line_number = 1;
+  while (getline(stream1, lineFrom1) && getline(stream2, lineFrom2)) {
+    if (lineFrom1 != lineFrom2) {
+      std::cout << "file1: " << line_number << ": " << lineFrom1 << std::endl;
+      std::cout << "file2: " << line_number << ": " << lineFrom2 << std::endl;
+      return 0;
+    }
+    ++line_number;
+  }
 }
