46d2ad77e937499a625b4de84730da6f8f7764de
Zaz Brown <zazbrown@zazbrown.com>
2023-06-18 07:52:20 +0000

    + skeleton for lab 2

---
+++ Lab2/wordCount.cpp
@@ -0,0 +1,46 @@
+//
+// Counts how many times each word occurs in a file.
+// Words are separated by whitespace or punctuation. Case is ignored.
+//
+// Zaz Brown
+//
+
+#include <iostream>
+#include <fstream>
+// TODO include multiset
+
+void error(std::string message) {
+  std::cout << "ERROR: " << message << std::endl;
+  // XXX: Does exit call destructors on everything in main?
+  exit(1);
+}
+
+int main(int argc, char* argv[]){
+  // throw an error if we are not given two command line arguments
+  // XXX: Is it safe to assume argc == argv.size()?
+  if (argc != 1) error("You must specify 2 files to diff");
+
+  const std::string file = argv[1];
+
+  // Open the files
+  // XXX: Is there a better way than .fail() to check if the file is readable?
+  std::ifstream stream(file);
+  if (stream.fail()) error("Could not open file " + file);
+
+  // create a bag
+
+  // until we reach the end of one of the files
+  // TODO create multiset
+  // TODO: more idiomatic way of doing this: while(getline) ?
+  while (!stream.eof()) {
+    std::string line;
+    getline(stream, line);
+    // TODO: downcase
+    // TODO: split into words
+    // TODO: add words to bag
+  }
+
+  // TODO: output word counts
+
+  if (stream.bad()) error("While reading file " + file);
+}
