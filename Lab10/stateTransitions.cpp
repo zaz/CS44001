1470b5f82c6462b5cfe02574a29059b63ec56d04
Zaz Brown <zazbrown@zazbrown.com>
2023-08-05 00:15:04 +0000

    State Transitions: add all states

--- Lab10/stateTransitions.cpp
+++ Lab10/stateTransitions.cpp
@@ -16,6 +16,9 @@ public:
    // behaviors
    void suspend();
    void dispatch();
+   void block();
+   void unblock();
+   void exit();
    string report();
 
    // part of implementation of state pattern
@@ -28,28 +31,17 @@ private:
 class State {
 public:
    // default to printing "Invalid action."
-   virtual void suspend(Process*)  { std::cerr << "Invalid action.\n"; }
+   virtual void  suspend(Process*) { std::cerr << "Invalid action.\n"; }
    virtual void dispatch(Process*) { std::cerr << "Invalid action.\n"; }
+   virtual void    block(Process*) { std::cerr << "Invalid action.\n"; }
+   virtual void  unblock(Process*) { std::cerr << "Invalid action.\n"; }
+   virtual void     exit(Process*) { std::cerr << "Invalid action.\n"; }
 
    virtual string report() = 0;
    void changeState(Process* c, State* s) {
       c->changeState(s);
    }
 };
-
-class Running: public State {
-public:
-   static State* instance() {
-      static State* onlyInstance = new Running;
-      return onlyInstance;
-   }
-   void suspend(Process*) override;
-   string report() override {return "running";}
-private:
-   // here and elsewhere should be stated private constructor/assignment
-   // to correctly implement singleton, skipped to simplify code
-};
-
 
 class Ready: public State {
 public:
@@ -61,6 +53,30 @@ public:
    string report() override {return "ready";}
 };
 
+class Running: public State {
+public:
+   static State* instance() {
+      static State* onlyInstance = new Running;
+      return onlyInstance;
+   }
+   void suspend(Process*) override;
+   void   block(Process*) override;
+   void    exit(Process*) override;
+   string report() override {return "running";}
+private:
+   // here and elsewhere should be stated private constructor/assignment
+   // to correctly implement singleton, skipped to simplify code
+};
+
+class Blocked: public State {
+public:
+   static State* instance() {
+      static State* onlyInstance = new Blocked;
+      return onlyInstance;
+   }
+   void unblock(Process*) override;
+   string report() override {return "blocked";}
+};
 
 class Dead: public State {
 public:
@@ -74,7 +90,7 @@ public:
 
 // state transitions member functions
 
-void Running::suspend(Process *c) {
+void Blocked::unblock(Process *c) {
    changeState(c, Ready::instance());
 }
 
@@ -82,12 +98,27 @@ void Ready::dispatch(Process *c) {
    changeState(c, Running::instance());
 }
 
+void Running::suspend(Process *c) {
+   changeState(c, Ready::instance());
+}
+
+void Running::block(Process *c) {
+   changeState(c, Blocked::instance());
+}
+
+void Running::exit(Process *c) {
+   changeState(c, Dead::instance());
+}
+
 // Process member functions
 Process::Process() {state_=Ready::instance();}
 
 // handles/behaviors
-void Process::suspend() {state_->suspend(this);}
+void  Process::suspend() {state_-> suspend(this);}
 void Process::dispatch() {state_->dispatch(this);}
+void    Process::block() {state_->   block(this);}
+void  Process::unblock() {state_-> unblock(this);}
+void     Process::exit() {state_->    exit(this);}
 string Process::report() {return state_->report();}
 
 int main() {
@@ -95,12 +126,20 @@ int main() {
    cout << "Zork is " << zork.report() << endl;
 
    while(zork.report() != "dead"){
-      cout << "What would you like Zork to do? dispatch or suspend? [d/s] ";
+      cout << "What would you like Zork to do? Dispatch, suspend, block, unblock, or exit? [d/s/b/u/x] ";
       char action; cin >> action;
-      if(action == 'd')
+      if (action == 'd')
          zork.dispatch();
-      else
+      else if (action == 's')
          zork.suspend();
+      else if (action == 'b')
+         zork.block();
+      else if (action == 'u')
+         zork.unblock();
+      else if (action == 'x')
+         zork.exit();
+      else
+         std::cerr << "Invalid action.\n" << std::flush;
       cout << "Zork is " << zork.report() << endl;
    }
 
