1985d085372f4ff455ee0b4d50c7558f1d30ff84
Zaz Brown <zazbrown@zazbrown.com>
2023-08-07 05:12:05 +0000

    add skeleton for concurrent coffee shack

---
+++ Lab11/coffeeShackConcurrent.cpp
@@ -0,0 +1,79 @@
+// Coffee Shack
+// Zaz Brown
+
+#include <iostream>
+#include <vector>
+#include "Drink.hpp"
+#include "Barista.hpp"
+
+void customerWalksIn() {
+    std::cout << "Welcome to Coffee Shack, can I get you [l]arge, [m]edium, or"
+              << " [s]mall coffee? ";
+    char size;
+    std::cin >> size;
+
+    DrinkSize myDrinkSize;
+    switch (size) {
+        case 'l':
+            myDrinkSize = DrinkSize::large;
+            break;
+        case 'm':
+            myDrinkSize = DrinkSize::medium;
+            break;
+        case 's':
+            myDrinkSize = DrinkSize::small;
+            break;
+        default:
+            std::cout << "Sorry, we don't have that size." << std::endl;
+            exit(1);
+    }
+
+    // I'm using pointers and new statements
+    // I hope you're proud of me
+    Drink* baseDrink = new Drink(myDrinkSize);
+
+    DrinkWithAddition* myDrink = new DrinkWithAddition(baseDrink);
+
+    // set up the Chain of Command
+    Barista *barista = new JuniorBarista(new SeniorBarista(new Manager));
+
+    char ingredient;
+    do {
+        std::cout << "Would you like to add [s]ugar, [c]ream, [h]oney, or [d]one? ";
+        std::cin >> ingredient;
+        switch (ingredient) {
+            case 's':
+                myDrink = barista->add(myDrink, "sugar");
+                break;
+            case 'c':
+                myDrink = barista->add(myDrink, "cream");
+                break;
+            case 'h':
+                myDrink = barista->add(myDrink, "honey");
+                break;
+            case 'd':
+                break;
+            default:
+                std::cout << "Sorry, we don't have that ingredient." << std::endl;
+                exit(1);
+        }
+    } while (ingredient != 'd');
+
+    std::cout << "Can I get your name? ";
+    std::string name;
+    std::cin >> name;
+    myDrink->setCustomerName(name);
+
+    std::cout << myDrink->getCustomerName()
+              << ", your "
+              << myDrink->getDescription()
+              << " is ready. It will be $"
+              << myDrink->getPrice()
+              << ", please."
+              << std::endl;
+}
+
+int main() {
+    customerWalksIn();
+    return 0;
+}
