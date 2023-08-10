9a61bb3c99b42a49176a3b8c9db5df0949fba9f6
Zaz Brown <zazbrown@zazbrown.com>
2023-08-10 19:21:31 +0000

    clean up whitespace

--- Lab12/history.cpp
+++ Lab12/history.cpp
@@ -21,58 +21,58 @@ public:
    void insert(int line, const string &str) {
       const int index = line-1;
       if (index <= lines_.size())
-	 lines_.insert(lines_.begin() + index, str); 
+         lines_.insert(lines_.begin() + index, str);
       else
-	 cout << "line out of range" << endl; 
+         cout << "line out of range" << endl;
    }
 
    const string remove(int line) {
       const int index = line-1;
       string deletedLine="";
       if(index < lines_.size()){
-	 deletedLine = lines_[index];
-	 lines_.erase(lines_.begin() + index); 
+         deletedLine = lines_[index];
+         lines_.erase(lines_.begin() + index);
       }else
-	 cout << "line out of range" << endl; 
+         cout << "line out of range" << endl;
       return deletedLine;
-   } 
+   }
 
    void show() {
       for(int i = 0; i < lines_.size(); ++i)
-	 cout << i + 1 << ". " << lines_[i] << endl; 
+         cout << i + 1 << ". " << lines_[i] << endl;
    }
 
 private:
-   vector<string> lines_; 
-}; 
+   vector<string> lines_;
+};
 
 // abstract command
 class Command{
 public:
    Command(Document *doc) : doc_(doc){}
-   virtual void execute() = 0; 
-   virtual void unexecute() = 0; 
+   virtual void execute() = 0;
+   virtual void unexecute() = 0;
    virtual ~Command(){}
 protected:
-   Document *doc_; 
-}; 
+   Document *doc_;
+};
 
 // two concrete commands
 class InsertCommand : public Command {
 public:
-   InsertCommand(Document *doc, int line, const string &str): 
+   InsertCommand(Document *doc, int line, const string &str):
       Command(doc), line_(line),  str_(str) {}
    void execute() override { doc_->insert(line_, str_);}
    void unexecute() override {doc_->remove(line_);}
 private:
-   int line_; 
-   string str_; 
-}; 
+   int line_;
+   string str_;
+};
 
 
 class EraseCommand : public Command {
 public:
-   EraseCommand(Document *doc, int line): 
+   EraseCommand(Document *doc, int line):
       Command(doc), line_(line), str_("") {}
    void execute() override {str_ = doc_->remove(line_);}
    void unexecute() override {doc_->insert(line_, str_);}
@@ -87,9 +87,9 @@ public:
    DocumentWithHistory(const vector<string> & text={}) : doc_(text){}
 
    void insert(int line, string str) {
-      Command *com = new InsertCommand(&doc_, line, str); 
-      com->execute(); 
-      doneCommands_.push(com); 
+      Command *com = new InsertCommand(&doc_, line, str);
+      com->execute();
+      doneCommands_.push(com);
    }
 
    void erase(int line){
@@ -100,58 +100,58 @@ public:
 
    void undo() {
       if(doneCommands_.size() != 0) {
-	 Command *com = doneCommands_.top(); 
-	 doneCommands_.pop(); 
-	 com->unexecute();
-	 delete com; // don't forget to delete command
+         Command *com = doneCommands_.top();
+         doneCommands_.pop();
+         com->unexecute();
+         delete com; // don't forget to delete command
       }else
-	 cout << "no commands to undo" << endl; 
+         cout << "no commands to undo" << endl;
    }
 
    void show() {doc_.show();}
 
 private:
    Document doc_;
-   std::stack<Command*> doneCommands_; 
-}; 
+   std::stack<Command*> doneCommands_;
+};
 
 // invoker
 int main() {
    DocumentWithHistory his({
-	 "Lorem Ipsum is simply dummy text of the printing and typesetting",
-	 "industry. Lorem Ipsum has been the industry's standard dummy text",
-	 "ever since the 1500s, when an unknown printer took a galley of",
-	 "type and scrambled it to make a type specimen book. It has",
-	 "survived five centuries."}); 
+         "Lorem Ipsum is simply dummy text of the printing and typesetting",
+         "industry. Lorem Ipsum has been the industry's standard dummy text",
+         "ever since the 1500s, when an unknown printer took a galley of",
+         "type and scrambled it to make a type specimen book. It has",
+         "survived five centuries."});
 
    char option;
    do{
-      his.show(); 
+      his.show();
       cout << endl;
 
       cout << "Enter option (i)nsert line, (e)rase line, (u)ndo last command: ";
-      cin >> option; 
+      cin >> option;
 
-      int line; string str; 
+      int line; string str;
       switch(option) {
       case 'i':
-	 cout << "line number to insert: "; 
-	 cin >> line; 
-	 cout << "line to insert: "; 
-	 cin.get(); // removes end-of-line character so getline() works correctly
-	 getline(cin, str); 
-	 his.insert(line, str); 
-	 break;
- 
+         cout << "line number to insert: ";
+         cin >> line;
+         cout << "line to insert: ";
+         cin.get(); // removes end-of-line character so getline() works correctly
+         getline(cin, str);
+         his.insert(line, str);
+         break;
+
       case 'e':
-	 cout << "line number to remove: ";
-	 cin >> line;
-	 his.erase(line);
-	 break;
+         cout << "line number to remove: ";
+         cin >> line;
+         his.erase(line);
+         break;
 
       case 'u':
-	 his.undo(); 
-	 break; 
+         his.undo();
+         break;
       }
 
    }while(option == 'i' || option == 'e' || option == 'u');
--- Lab12/snapshot.cpp
+++ Lab12/snapshot.cpp
@@ -17,39 +17,39 @@ public:
    void insert(int line, const string &str) {
       int index = line-1;
       if (index <= lines_.size()+1)
-	 lines_.insert(lines_.begin()+index, str); 
+         lines_.insert(lines_.begin()+index, str);
       else
-	 cout << "line out of range" << endl; 
+         cout << "line out of range" << endl;
    }
 
    void remove(int line) {
       int index = line-1;
       if(index < lines_.size())
-	 lines_.erase(lines_.begin()+index); 
+         lines_.erase(lines_.begin()+index);
       else
-	 cout << "line out of range" << endl; 
-   } 
+         cout << "line out of range" << endl;
+   }
 
    void show() {
       for(int i = 0; i < lines_.size(); ++i)
-	 cout << i + 1 << ". " << lines_[i] << endl; 
+         cout << i + 1 << ". " << lines_[i] << endl;
    }
 
-   class Memento *createMemento() const;  
-   void rollBack(class Memento *); 
+   class Memento *createMemento() const;
+   void rollBack(class Memento *);
 
 private:
-   vector<string> lines_; 
-}; 
+   vector<string> lines_;
+};
 
 class Memento {
 public:
    Memento(const Document& doc): doc_(doc) {}
-   const Document& getState() const {return doc_;}  
+   const Document& getState() const {return doc_;}
 private:
    const Document doc_;
 };
- 
+
 Memento *Document::createMemento() const{
    return new Memento(*this); // copying the Document itself
 }
@@ -65,11 +65,11 @@ int main() {
          "Lorem Ipsum is simply dummy text of the printing and typesetting",
          "industry. Lorem Ipsum has been the industry's standard dummy text",
          "ever since the 1500s, when an unknown printer took a galley of",
-	 "type and scrambled it to make a type specimen book. It has",
-	 "survived five centuries."});
+         "type and scrambled it to make a type specimen book. It has",
+         "survived five centuries."});
 
    char option;
-   
+
    do{
       static Memento *checkpoint = nullptr;
 
@@ -79,13 +79,13 @@ int main() {
       cout << "Enter option (i)nsert line, (e)rase line, (c)heckpoint, (r)ollback: ";
       cin >> option;
 
-      int line; string str; 
+      int line; string str;
       switch(option) {
       case 'i':
          cout << "line number to insert: ";
          cin >> line;
          cout << "line to insert: ";
-	 cin.get();
+         cin.get();
          getline(cin, str);
          doc.insert(line, str);
          break;
@@ -95,15 +95,15 @@ int main() {
          doc.remove(line);
          break;
       case 'c':
-	 checkpoint = doc.createMemento();
-	 break;
+         checkpoint = doc.createMemento();
+         break;
       case 'r':
-	 if (checkpoint != nullptr){
-	    doc.rollBack(checkpoint);
-	    delete checkpoint;
-	    checkpoint = nullptr;
-	 }else
-	    cout << "No checkpoint created" << endl;
+         if (checkpoint != nullptr){
+            doc.rollBack(checkpoint);
+            delete checkpoint;
+            checkpoint = nullptr;
+         }else
+            cout << "No checkpoint created" << endl;
          break;
       }
    }while(option == 'i' || option == 'e' || option == 'c' || option == 'r');
